4a539ca497638fbf1ac049514fc8d737b30f1ddc
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 21:08:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,23 +1,11 @@
 def my_collect(project)
   x = 0
   collection = []
-  
-
-
-
-
-
-
-
-
-
-
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
+  while x < project.length
+    collection << yield(project[x])
+    x += 1
+  end 
   collection
-end
+end 
+
 
